d8a4cfddd71519d9a27258931b13e4ec019d6e4b
Fyamada1229 <yamadafutosi1229@gmail.com>
2022-03-19 23:08:09 +0900

    [add] 実装完了

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCatsFetch } from "./catState";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+  console.log(cats);
+
+  return (
+    <>
+      <div className="App">
+        <h1>CATS</h1>
+        <p>Cat Pictures</p>
+        <hr />
+        <div className="Gallery">
+          {cats.map((cat) => (
+            <div key={cat.id} className="row">
+              <div className="column column-right">
+                <img
+                  alt="cat.name"
+                  src={cat.image.url}
+                  width="200"
+                  height="200"
+                />
+              </div>
+              <div className="column column-left">
+                <h2>{cat.name}</h2>
+                <h5>Temperament: {cat.temperament}</h5>
+                <p>{cat.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button>VIEW MORE CATS</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-
 import createSagaMiddleware from "redux-saga";
 import { configureStore } from "@reduxjs/toolkit";
+import catsReducer from "./catState";
+import catSaga from "./catSaga";
 
 const saga = createSagaMiddleware();
 const store = configureStore({
-  reducer: [],
+  reducer: {
+    cats: catsReducer,
+  },
   middleware: [saga],
 });
+saga.run(catSaga);
 
 ReactDOM.render(
   <Provider store={store}>
